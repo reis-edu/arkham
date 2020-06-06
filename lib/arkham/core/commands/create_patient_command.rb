ef002130ae17b96f7de45d783b98d53ddb472ebf
Edu Reis <edureis.dev@hotmail.com>
2020-06-06 13:35:40 -0300

    Remove useless method

--- lib/arkham/core/commands/create_patient_command.rb
+++ lib/arkham/core/commands/create_patient_command.rb
@@ -4,16 +4,16 @@ module Core
       attr_accessor :patient
 
       def initialize(patient_params:)
-        set_patient(patient_params)
+        @patient = set_patient(patient_params)
       end
 
       private
  
       def set_patient(patient_params)
         patient_validation = Api::PatientSchema.call(patient_params)
-        raise Api::Errors::SchemaValidationError, patient_validation.errors if patient_validation.errors.any?
+        return patient_validation.output unless patient_validation.errors.any?
 
-        @patient = patient_validation.output
+        raise Api::Errors::SchemaValidationError, patient_validation.errors
       end
     end
   end
