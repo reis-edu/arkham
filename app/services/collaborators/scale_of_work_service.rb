4b5956c3df76410368372a3d743e81f630d16b04
Edu Reis <edureis.dev@hotmail.com>
2020-10-25 19:05:15 -0300

    Return fullname attribute on get patients list

---
+++ app/services/collaborators/scale_of_work_service.rb
@@ -0,0 +1,18 @@
+module Collaborators
+  class ScaleOfWorkService
+    
+    def create(month, year = Time.current.year)
+      reference_date = Time.new(year, month, 1)
+    end
+
+    def find(month, year = Time.current.year)
+      raise NotImplementedError
+    end
+
+    private
+
+    def days_of_month(reference_date)
+      (reference_date.beginning_of_month..reference_date.end_of_month).map(&:mday)
+    end
+  end
+end
